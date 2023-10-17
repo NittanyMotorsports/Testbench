5cb5d4d1f92bb2cfd9a641b8ee853e1f7923fb5b
Linux machine <kjg5690@psu.edu>
2023-10-17 19:33:20 -0400

    Added an example gpio sketch.

---
+++ HIL/ExampleCode/rpi_gpio_blink.py
@@ -0,0 +1,22 @@
+"""
+This is an example blink sketch brought to you by ChatGPT
+
+Follow this layout for how to control the GPIOs on the rpi
+"""
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)  # Use BCM numbering for GPIO pins
+
+# Set up the GPIO pin
+led_pin = 17
+GPIO.setup(led_pin, GPIO.OUT)
+
+# Turn the LED on and off
+GPIO.output(led_pin, GPIO.HIGH)  # Turn on
+time.sleep(2)
+GPIO.output(led_pin, GPIO.LOW)   # Turn off
+
+# Clean up
+GPIO.cleanup()
