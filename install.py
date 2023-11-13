36f06f12a2bb1b28c6eb663141fdd6c1b27a7350
Kyle <kjg5690@psu.edu>
2023-11-13 14:06:00 -0500

    Beginning of install script, need to test.

--- install.py
+++ install.py
@@ -1,1 +1,13 @@
 # Install script to install all dependencies Linux machine will need
+
+import subprocess
+
+def installs():
+    try:
+        subprocess.check_call(['pip', 'install', 'python-can'])
+        print('Successfully installed python-can')
+    except subprocess.CalledProcessError as e:
+        print(f'Failed to install python-can. Error: {e}')
+
+if __name__ == '__main__':
+    installs()
