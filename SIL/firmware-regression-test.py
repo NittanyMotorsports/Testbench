9a8e717bdd92639acbc660ea468fb74c175df057
Linux machine <kjg5690@psu.edu>
2023-11-21 19:36:26 -0500

    Prelim firmware regression test.

---
+++ SIL/firmware-regression-test.py
@@ -0,0 +1,25 @@
+import can
+import os
+import slash
+
+os.system('sudo ifconfig can0 down')
+os.system('sudo ip link set can0 type can bitrate 500000')
+os.system('sudo ifconfig can0 up')
+
+bus = can.interface.Bus(channel="can0", bustype='socketcan')
+
+def test_MC_clear_faults():
+    pass
+
+def read() :
+    message = bus.recv(10.0)
+    print(message)
+
+try:
+    while True:
+        read()
+except:
+    pass
+
+finally:
+    os.system('sudo ifconfig can0 down')
