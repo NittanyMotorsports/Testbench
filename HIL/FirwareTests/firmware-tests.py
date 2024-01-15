0b98f6bee42ec47dad92e9ddcf5e8f6990fbd94a
Thomas Gillen <thg5077@psu.edu>
2024-01-15 15:18:19 -0500

    Initial commit for APPS test

--- HIL/FirwareTests/firmware-tests.py
+++ HIL/FirwareTests/firmware-tests.py
@@ -13,6 +13,8 @@ RPi_GPIOs = {
     "BrakesLeft": Pin(11, GPIO.OUT),
     "BrakesRight": Pin(13, GPIO.OUT),
     "Buzzer": Pin(15, GPIO.IN),
+    "APPSLeft": Pin(17, GPIO.OUT),
+    "APPSRight": Pin(19, GPIO.OUT)
 }
 
 def test_ready_to_drive():
@@ -35,6 +37,32 @@ def test_ready_to_drive():
 
     # Step 6: Reset all pins to put STM32F4 back in idle state
     pass
+
+def test_APPS():
+
+    #Output GPIO_HIGH on RPi to both APPS inputs on STM
+
+    GPIO.output(RPi_GPIOs["APPSLeft"].pin_num, GPIO.HIGH)
+    GPIO.output(RPi_GPIOs["APPSRight"].pin_num, GPIO.HIGH)
+
+    #Check that throttle CAN message is greater than 2^15
+
+    #if CAN.read > 32768:
+        #pass
+    #else:
+        #pass
+
+    #Switch one APPS input to GPIO_LOW while other is GPIO_HIGH
+
+    GPIO.output(RPi_GPIOs["APPSRight"].pin_num, GPIO.LOW)
+
+    #Check that throttle CAN message is zero
+
+    #if CAN.read == 0:
+        #pass
+    #else:
+        #pass
+
 
 def configure_RPi():
     """ Sets up the RPi for all tests. Connects to CAN bus and configures GPIOs. """
