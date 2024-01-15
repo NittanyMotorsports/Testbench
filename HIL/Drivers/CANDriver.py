9a3253bfa63b38aea7c960db8d699f01d5a729b6
Rishab Srinivasan <rks6004@psu.edu>
2024-01-15 15:25:16 -0500

    Initial commit for CAN driver

---
+++ HIL/Drivers/CANDriver.py
@@ -0,0 +1,42 @@
+# from base_CAN_driver import BaseCANDriver
+
+import can
+import os
+import time
+from typing import List, Union
+
+class CANableDriver:
+    def __init__(self, CAN_channel: str="can0", baud_rate: int=500_000, bus_type: str="socketcan") -> None:
+        self.CAN_channel = CAN_channel
+        self.baud_rate = baud_rate
+        self.bus_type = bus_type
+        self.bus = None
+
+    def connect(self):
+        os.system('sudo ifconfig can0 down')
+        os.system('sudo ip link set can0 type can bitrate 500000')
+        os.system('sudo ifconfig can0 up')
+        self.bus = can.interface.Bus(channel=self.CAN_channel, interface=self.bus_type, baud_rate = self.baud_rate)
+
+        
+
+    def read(self, delay_ms: Union[float, int]):
+        assert(self.bus != None, "Connection not instantiated")
+        message = self.bus.recv(delay_ms)
+        print(message)
+        
+
+    def write(self, id: int, data: List[int]):
+        assert(self.bus != None, "Connection not instantiated")
+        can_m = can.Message(arbitration_id = id, data = data)
+        self.bus.send(can_m)
+        
+
+    def wait_until_id(self,  id: int, timeout_s: Union[float, int]=20) -> Union[List[Union[float, int]], bool]:
+        assert(self.bus != None, "Connection not instantiated")
+        pass
+
+    def disconnect(self):
+        assert(self.bus != None, "Connection not instantiated")
+        os.system('sudo ifconfig can0 down')
+        pass
