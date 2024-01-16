337ae8e7d63d30ab1d2e79cbc6c4c1cbe6b32eea
Kyle <kjg5690@psu.edu>
2024-01-16 18:38:49 -0500

    Initial commit for test of motor controller clear faults function.

--- HIL/FirwareTests/firmware-tests.py
+++ HIL/FirwareTests/firmware-tests.py
@@ -36,6 +36,33 @@ def test_ready_to_drive():
     # Step 6: Reset all pins to put STM32F4 back in idle state
     pass
 
+def test_motor_controller_fault():
+    """
+    This tests the functionality of the clear faults button in the cockpit of the vehicle.
+    If a over voltage, over current, or over speed fault occur within the Motor Controller (MC)
+    then the MC sends a message over CAN indicating that a fault occured. An LED driven by the
+    STM32F4 should turn on if this message is received. Once the clear faults button is pressed
+    the STM32F4 should send a message over CAN to the MC indicating to reset itself. The LED that
+    is being driven by the STM32F4 should also turn off.
+    """
+
+    # Step 1: Check that the throttle message from the STM32F4 is able to be received
+
+    # Step 2: Send the CAN message indicating that the fault has occured (id = 0x0AB, data = [0,0,0,0,3,0,0,0])
+
+    # Step 3: Allow time for STM32F4 to receive CAN message and process the data
+
+    # Step 4: Check that the LED driven by the STM32F4 turned on
+
+    # Step 5: Turn on GPIO on RPi for the clear fault button
+
+    # Step 6: Check that the STM32F4 sends the CAN message to clear faults (id = 0x0C1)
+
+    # Step 7: Check that the LED turns off after the button is pressed
+
+    # Step 8: Reset all pins to put STM32F4 back in idle state
+    pass
+
 def configure_RPi():
     """ Sets up the RPi for all tests. Connects to CAN bus and configures GPIOs. """
     GPIO.setmode(GPIO.BOARD)
