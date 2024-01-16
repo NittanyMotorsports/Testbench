a7b8311e4d385d1c626307fc0088887481dfcbbe
Kyle <kjg5690@psu.edu>
2024-01-15 23:09:00 -0500

    Added comments and finsihed driver.

--- HIL/Drivers/CANDriver.py
+++ HIL/Drivers/CANDriver.py
@@ -13,6 +13,7 @@ class CANableDriver:
         self.bus = None
 
     def connect(self):
+        """ Starts up the CAN bus connection and creates the bus interface. """
         os.system('sudo ifconfig can0 down')
         os.system('sudo ip link set can0 type can bitrate 500000')
         os.system('sudo ifconfig can0 up')
@@ -21,22 +22,30 @@ class CANableDriver:
         
 
     def read(self, delay_ms: Union[float, int]):
+        """ Reads the bus and returns the first message received. """
         assert(self.bus != None, "Connection not instantiated")
         message = self.bus.recv(delay_ms)
-        print(message)
+        return message
         
 
     def write(self, id: int, data: List[int]):
+        """ Writes specified message to the bus. """
         assert(self.bus != None, "Connection not instantiated")
         can_m = can.Message(arbitration_id = id, data = data)
         self.bus.send(can_m)
         
 
     def wait_until_id(self,  id: int, timeout_s: Union[float, int]=20) -> Union[List[Union[float, int]], bool]:
+        """ Reads the bus and returns the message with given id or times out after specified duration. """
         assert(self.bus != None, "Connection not instantiated")
-        pass
+        start = time.time()
+        while time.time() - start < timeout_s:
+            message = self.read(1.0)
+            if message.arbitration_id == id:
+                return message
+        return None
 
     def disconnect(self):
+        """ Disconnects from the CAN bus. """
         assert(self.bus != None, "Connection not instantiated")
         os.system('sudo ifconfig can0 down')
-        pass
